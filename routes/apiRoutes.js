54eb16086cb61c233154f8ea55062f76562105ac
Joey <joewoodnyc@gmail.com>
2021-06-22 18:00:36 -0400

    FIXING DEPLOYMENT

---
+++ routes/apiRoutes.js
@@ -0,0 +1,11 @@
+const noteData = require('../db/db');
+
+module.exports = (app) => {
+
+    app.get('/api/notes', (req, res) => res.json(noteData));
+
+    app.post('/api/notes', (req, res) => {noteData.push(req.body)} );
+
+    app.post('/api/clear', (req,res) => {noteData.length = 0} );
+
+}
