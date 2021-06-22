99d07b53e9d6fd41edbce816cea8c88b7502ddc7
Joey <joewoodnyc@gmail.com>
2021-06-22 18:02:48 -0400

    adding db.js

---
+++ db/db.js
@@ -0,0 +1,12 @@
+const notesArray = [
+    {
+        title:"Grocery List",
+        text:"Lemons, apples, pears"
+    },
+    {
+        title:"Shopping List",
+        text:"plates, napkins, forks"
+    }
+]
+
+module.exports = notesArray;
